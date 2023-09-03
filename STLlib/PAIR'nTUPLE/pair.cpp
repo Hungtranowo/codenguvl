af616b353766fd28ee0cf0baf908da60ac4f8900
Hungtranowo <hungtrantuan20080@gmail.com>
2023-09-03 14:24:21 +0700

    delete some files

--- STLlib/PAIR'nTUPLE/pair.cpp
+++ STLlib/PAIR'nTUPLE/pair.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(){
     // declare PAIR 
     //pair <datatype1, datatype2> namePair;
-    pair <int, int> time = {0, 10};
+    pair <int, int> timepr = {0, 10};
     pair <string, int> nameAge = {"Hung", 15};
 
-    cout << time.first << " " << time.second << endl;
+    cout << timepr.first << " " << timepr.second << endl;
     cout << nameAge.first << " " << nameAge.second << endl; 
 
     return 0;
