795d5af0d41793b72167e155034416f1df7c5686
Hungtranowo <hungtrantuan20080@gmail.com>
2023-12-09 20:19:59 +0700

    all

--- NTUcoder/B10fibonacii.cpp
+++ NTUcoder/B10fibonacii.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-using namespace std;
+using namespace std
+
+unsigned long long fibo(unsigned long long n){
+    if (n==1 || n==2) return 1;
+    return n + fibo(n-1);
+}
 
 int main(){
-    int n; cin >> n;
-       
+    unsigned long long n; cin >> n;
+    cout << fibo(n);
     return 0;
 }
